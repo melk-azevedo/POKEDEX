c8d7ab4d9ee828be5a40fdabb273665073a88a71
Melk Azevedo <melk.azevedo@outlook.com>
2023-02-20 21:55:18 -0300

    aumentando a quantidade de pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
-const maxRecords = 151
-const limit = 10
-let offset = 0;
+const maxRecords = 200
+const limit = 12
+let offset = 0
 
 function convertPokemonToLi(pokemon) {
     return `
